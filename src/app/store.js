5d583678a281d98b5b4075d701cdfb97a4906c13
Dragos Dogaru <ryu.okeya@gmail.com>
2023-01-09 10:58:53 +0900

    added localstorage persistence

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,41 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import deckReducer from "../features/decks/deckSlice";
 import noteReducer from "../features/notes/noteSlice";
+import storage from "redux-persist/lib/storage";
+import { combineReducers } from "redux";
+import {
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
 
+//object cfg that will be stored in storage
+const persistConfig = {
+  key: "AnkiManager",
+  storage,
+};
+
+//combine all reducers
+const reducers = combineReducers({
+  decks: deckReducer,
+  notes: noteReducer,
+});
+
+//create a persistent reducer
+const persistentReducer = persistReducer(persistConfig, reducers);
+
+//assign the persistent reducer in the store cfg and add
+//the dispatch functions to the serializable checks ignore list
 export const store = configureStore({
-  reducer: {
-    decks: deckReducer,
-    notes: noteReducer,
-  },
+  reducer: persistentReducer,
+  middleware: () =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
--- src/index.js
+++ src/index.js
@@ -5,12 +5,21 @@ import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
 import { store } from "./app/store";
+import { persistStore } from "redux-persist";
+import { PersistGate } from "redux-persist/integration/react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
+let persistor = persistStore(store);
+
+//wrap whole app with Redux Provider (for global state designation)
+//we also wrap the app with PersistGate - needed for keeping data in localStorage
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
