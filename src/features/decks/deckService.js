d9ebbdd90b69808e97538b3de199bff0f80a0df7
drag0sd0g <ryu.okeya@gmail.com>
2023-01-09 19:21:07 +0900

    cleanup redundancies

--- src/features/decks/deckService.js
+++ src/features/decks/deckService.js
@@ -2,40 +2,22 @@ import axios from "axios";
 
 // Get all deck names and IDs for current Anki user
 const getDeckNamesAndIds = async () => {
-  const config = {
-    headers: {
-      "Content-Type": "application/json",
-    },
-  };
   const postData = {
     action: "deckNamesAndIds",
     version: parseInt(process.env.REACT_APP_ANKI_API_VERSION),
   };
-  const response = await axios.post(
-    process.env.REACT_APP_ANKI_URL,
-    postData,
-    config
-  );
+  const response = await axios.post(process.env.REACT_APP_ANKI_URL, postData);
   return response.data;
 };
 
 //given an array of deck names, get all stats for each
 const getDeckStats = async (deckNameArr) => {
-  const config = {
-    headers: {
-      "Content-Type": "application/json",
-    },
-  };
   const postData = {
     action: "getDeckStats",
     version: parseInt(process.env.REACT_APP_ANKI_API_VERSION),
     params: { decks: deckNameArr },
   };
-  const response = await axios.post(
-    process.env.REACT_APP_ANKI_URL,
-    postData,
-    config
-  );
+  const response = await axios.post(process.env.REACT_APP_ANKI_URL, postData);
   return response.data;
 };
 
--- src/features/notes/noteService.js
+++ src/features/notes/noteService.js
@@ -2,52 +2,28 @@ import axios from "axios";
 
 //Get all note IDs from a given deck
 const getNotesForDeck = async (deckId) => {
-  const config = {
-    headers: {
-      "Content-Type": "application/json",
-    },
-  };
   const postData = {
     action: "findNotes",
     version: parseInt(process.env.REACT_APP_ANKI_API_VERSION),
     params: { query: "did:".concat(deckId) },
   };
-  const response = await axios.post(
-    process.env.REACT_APP_ANKI_URL,
-    postData,
-    config
-  );
+  const response = await axios.post(process.env.REACT_APP_ANKI_URL, postData);
   return response.data;
 };
 
 //Get all note info
 const getNotesInfo = async (noteIdArr) => {
-  const config = {
-    headers: {
-      "Content-Type": "application/json",
-    },
-  };
   const postData = {
     action: "notesInfo",
     version: parseInt(process.env.REACT_APP_ANKI_API_VERSION),
     params: { notes: noteIdArr },
   };
-  const response = await axios.post(
-    process.env.REACT_APP_ANKI_URL,
-    postData,
-    config
-  );
+  const response = await axios.post(process.env.REACT_APP_ANKI_URL, postData);
   return response.data;
 };
 
 //add a note
 const addNote = async (deckName, expression, reading, meaning) => {
-  //prep headers
-  const config = {
-    headers: {
-      "Content-Type": "application/json",
-    },
-  };
   //prep payload
   const postData = {
     action: "addNote",
@@ -65,11 +41,7 @@ const addNote = async (deckName, expression, reading, meaning) => {
     },
   };
   // peform async call
-  const response = await axios.post(
-    process.env.REACT_APP_ANKI_URL,
-    postData,
-    config
-  );
+  const response = await axios.post(process.env.REACT_APP_ANKI_URL, postData);
   return response.data;
 };
 
